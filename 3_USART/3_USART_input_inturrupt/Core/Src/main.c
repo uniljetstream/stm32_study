9cb655e60ca8237f8149c84cca6b118443e9c106
uniljetstream <test@test.com>
2025-08-20 21:54:47 +0900

    uart it

--- 3_USART/3_USART_input_inturrupt/Core/Src/main.c
+++ 3_USART/3_USART_input_inturrupt/Core/Src/main.c
@@ -21,9 +21,10 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
 #include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
@@ -31,7 +32,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define TX_BUF_SIZE 16
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,7 +44,8 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+uint8_t rxData;
+uint8_t txBuf[TX_BUF_SIZE];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,51 +58,64 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+//USART2 RX에 인터럽트 요청이 있을 때 실행되는 Callback 함수
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+if (huart->Instance == USART2)
+{
+const char msg = "recv";
+for (int i=0;i<5;i++) {
+HAL_UART_Transmit(&huart2, (uint8_t)msg, strlen(msg), 100);
+}
+HAL_UART_Receive_IT(&huart2, &rxData, 1);
+}
+}
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+* @brief  The application entry point.
+* @retval int
+*/
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-	uint8_t size;
-	uint8_t message[] = "Transmit Test IT\n\r";
-	size = sizeof(message);
-  /* USER CODE END 1 */
+/* USER CODE BEGIN 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+/* USER CODE END 1 */
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+/* MCU Configuration--------------------------------------------------------*/
 
-  /* USER CODE BEGIN Init */
+/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+HAL_Init();
 
-  /* USER CODE END Init */
+/* USER CODE BEGIN Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+/* USER CODE END Init */
 
-  /* USER CODE BEGIN SysInit */
+/* Configure the system clock */
+SystemClock_Config();
 
-  /* USER CODE END SysInit */
+/* USER CODE BEGIN SysInit */
+
+/* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  //USART2 RX에 인터럽트를 시작한다.
+  HAL_UART_Receive_IT(&huart2, &rxData, 1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  /* 주기적으로 "in ing" 전송 (1초 간격) */
+  const char *periodic = "ining";
   while (1)
   {
-	  HAL_UART_Transmit_IT(&huart2, message, size);
-	  HAL_Delay(1000);
+	size_t len = strlen(periodic);
+	HAL_UART_Transmit(&huart2, (uint8_t*)periodic, (uint16_t)len, 100); /* 블로킹 전송 */
+	HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
